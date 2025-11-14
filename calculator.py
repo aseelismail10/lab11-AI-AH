5bee2fd8bdddee5b624ae27b4a3f11379bec6950
abbyhepburn <abbyhepburn@icloud.com>
2025-11-13 19:35:10 -0500

    modified calculator p2

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import math
+def add(a, b):
+    a + b
+
+def sub(a, b):
+    a - b
+
+def mul(a, b):
+    a * b
+
+def div(a, b):
+    try:
+        b / a
+    except:
+        raise "ZeroDivisionError"
+# raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    try:
+        math.log(a,b)
+    # use math library + raise ValueError
+    except:
+        raise ValueError
+def exp(a, b):
+    math.pow(a,b)
